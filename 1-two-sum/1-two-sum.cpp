96dad92051e3e3bd3baed8ce0bdda1f780edcd73
hhakung <hhakung@users.noreply.github.com>
2022-05-14 23:05:02 -0700

    Time: 548 ms (22.32%), Space: 10.2 MB (87.46%) - LeetHub

---
+++ 1-two-sum/1-two-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for( auto i { 0 }; i < nums.size() - 1; ++i )
+        {
+            for( auto j { i + 1 }; j < nums.size(); ++j )
+            {
+                if( nums[i] + nums[j] == target )
+                {
+                    return { i, j };
+                }
+            }
+        }
+        
+        return {};
+    }
+};
